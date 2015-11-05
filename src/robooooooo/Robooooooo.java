31209f5ac2e79a5c9d09889cd55f329a407b9020
SimonLaptop <SimonLaptop@SimonLaptop-PC>
2015-11-05 17:05:28 +0100

    test2

--- src/robooooooo/Robooooooo.java
+++ src/robooooooo/Robooooooo.java
@@ -16,6 +16,7 @@ public class Robooooooo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hallo Welt!");
     }
     
 }
